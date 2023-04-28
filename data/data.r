60811c63970796e7955101bdc8573a22b1327520
Mario Edoardo Pandolfo <jrhin@proton.me>
2023-04-28 19:35:12 +0200

    Add Deep Learning Project

--- data/data.r
+++ data/data.r
@@ -7,7 +7,7 @@ work <- tribble(
   ~ Title, ~ startMonth, ~ startYear, ~ endMonth, ~ endYear, ~ Where, ~ Desc,
   "Assistant Teacher", "Sept.", "2014", "June", "2016", "Wu Tao Kwoon Kung Fu, Rome, Italy", "I've assisted the teachers during their lessons.",
   "Teacher", "Sept.", "2016", "June", "2018", "Wu Tao Kwoon Kung Fu, Rome, Italy", "I was in charge of planning and managing lessons for children and teenagers courses. Each of the courses was 1 hour long twice a week with an average of 20 students.",
-  "Magician and Entertainer", "Oct.", "2018", NA, "occasionally", "bookstores, Rome, Italy", "Doing events for \"Il Coniglio Volante\", \"Bottega delle Storie\" and \"Storia e Magia\"." 
+  "Magician and Entertainer", "Oct.", "2018", "Oct.", "2022", "bookstores, Rome, Italy", "Doing events for \"Il Coniglio Volante\", \"Bottega delle Storie\" and \"Storia e Magia\"." 
 )
 
 certifications <- tribble(
@@ -28,4 +28,9 @@ competitions <- tribble(
   "World Traditional Kung Fu Championship", "Dec. 2014", "Rome, Italy", "I was the only one of my age in the category, so I had to compete with people over 20 years.",
   "\"Come suona la tua vita?\" writing contest", "2017", "Rome, Italy", "1° prize for the aphorism category.",
   "Hackathon organized for the Statistical Learning Course", "July 2022", "Rome, Italy", "The goal of this 48h Hackathon was to implement a model that was able to classify the \"heart rate zone\" from a dataset collected with an Apple Watch accompanied by a Polar OH1 sensor. I was the leader of our group, which was composed by three people of different nationalities, so to understand each other we had to talk in english." 
+)
+
+projects <- tribble(
+  ~ Name, ~ When, ~ Desc,
+  "Modified ESRGAN for solving the Super Resolution Problem", "Oct. 2022", "We have worked on a PyTorch Lightning implementation of the ESRGAN with the supplementary usage of the Wasserstein Loss. In addition we have introduced two modified models of ESRGAN: the ESRGAN+IR and the DRGAN."
 )
